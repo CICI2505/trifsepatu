a36f83ea4e98dde09becc3afd777fb3f54aaa7d5
CICI2505 <ciciyulita9@gmail.com>
2024-03-07 13:37:45 +0700

    ini adalah list barang

---
+++ app/Http/Controllers/ListBarangController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListBarangController extends Controller
+{
+    //
+}
+
+class ListBarangController extends Controller
+{
+    function tampilkan($id, $nama){
+        return view('list_barang', ['id' => $id, 'nama' => $nama]);
+    }
+}
